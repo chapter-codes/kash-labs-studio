5b0d23c518840730d8fa95dcc6f7b04ca25a3481
churchill nnanna <mrxyfactor@gmail.com>
2026-03-13 00:22:19 +0100

    initial commit

---
+++ schemaTypes/projectType.ts
@@ -0,0 +1,52 @@
+import {defineField, defineType} from 'sanity'
+
+export const projectType = defineType({
+  name: 'projects',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'projectName',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'projectDescription',
+      type: 'string',
+    //   validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'tags',
+      type: 'array',
+      of:[{type:'string'}],
+      validation:(rule)=>rule.required()
+    }),
+    defineField({
+      name: 'projectLink',
+      type: 'string',
+      validation:(rule)=>rule.required()
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+      validation:(rule)=>rule.required()
+    }),
+        defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    //  defineField({
+    //   name: 'slug',
+    //   type: 'slug',
+    //   options: {source: 'title'},
+    //   validation: (rule) => rule.required(),
+    // }),
+    // defineField({
+    //   name: 'body',
+    //   type: 'array',
+    //   of: [{type: 'block'}],
+    // }),
+  ],
+})
