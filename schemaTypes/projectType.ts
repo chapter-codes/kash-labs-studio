afc362f2c96ad422b1117966d836765cf8c6d789
churchill nnanna <mrxyfactor@gmail.com>
2026-03-18 12:30:55 +0100

    type change

--- schemaTypes/projectType.ts
+++ schemaTypes/projectType.ts
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export const projectType = defineType({
-  name: 'projects',
-  title: 'Projects',
+  name: 'project',
+  title: 'Project',
   type: 'document',
   fields: [
     defineField({
